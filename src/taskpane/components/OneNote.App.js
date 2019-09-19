867d468ef5878210a6f4bf75c9fef258bf862797
Courtney Owen <cowen@microsoft.com>
2019-09-19 09:30:53 -0700

    Merge office-addin-taskpane-react/master

--- src/taskpane/components/OneNote.App.js
+++ src/taskpane/components/OneNote.App.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { Button, ButtonType } from 'office-ui-fabric-react';
-import Header from './Header';
-import HeroList, { HeroListItem } from './HeroList';
-import Progress from './Progress';
+import * as React from "react";
+import { Button, ButtonType } from "office-ui-fabric-react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+/* global Button, Header, HeroList, HeroListItem, Progress */
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
@@ -16,16 +17,16 @@ export default class App extends React.Component{
     this.setState({
       listItems: [
         {
-          icon: 'Ribbon',
-          primaryText: 'Achieve more with Office integration'
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration"
         },
         {
-          icon: 'Unlock',
-          primaryText: 'Unlock features and functionality'
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality"
         },
         {
-          icon: 'Design',
-          primaryText: 'Create and visualize like a pro'
+          icon: "Design",
+          primaryText: "Create and visualize like a pro"
         }
       ]
     });
@@ -33,32 +34,34 @@ export default class App extends React.Component{
 
   click = async () => {
     /**
-    * Insert your OneNote code here
-    */
-  }
+     * Insert your OneNote code here
+     */
+  };
 
   render() {
-    const {
-      title,
-      isOfficeInitialized,
-    } = this.props;
+    const { title, isOfficeInitialized } = this.props;
 
     if (!isOfficeInitialized) {
       return (
-        <Progress
-          title={title}
-          logo='assets/logo-filled.png'
-          message='Please sideload your addin to see app body.'
-        />
+        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
       );
     }
 
     return (
-      <div className='ms-welcome'>
-        <Header logo='assets/logo-filled.png' title={this.props.title} message='Welcome' />
-        <HeroList message='Discover what Office Add-ins can do for you today!' items={this.state.listItems}>
-          <p className='ms-font-l'>Modify the source files, then click <b>Run</b>.</p>
-          <Button className='ms-welcome__action' buttonType={ButtonType.hero} iconProps={{ iconName: 'ChevronRight' }} onClick={this.click}>Run</Button>
+      <div className="ms-welcome">
+        <Header logo="assets/logo-filled.png" title={this.props.title} message="Welcome" />
+        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
+          <p className="ms-font-l">
+            Modify the source files, then click <b>Run</b>.
+          </p>
+          <Button
+            className="ms-welcome__action"
+            buttonType={ButtonType.hero}
+            iconProps={{ iconName: "ChevronRight" }}
+            onClick={this.click}
+          >
+            Run
+          </Button>
         </HeroList>
       </div>
     );
