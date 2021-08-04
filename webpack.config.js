6d31359fa54a9fbba51b75cf0998abba2d1d1247
Darren Miller <darrmill@microsoft.com>
2021-08-04 09:59:56 -0700

    Merge Shared Changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,28 @@
+/* eslint-disable no-undef */
+
 const devCerts = require("office-addin-dev-certs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const urlDev="https://localhost:3000/";
-const urlProd="https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlDev = "https://localhost:3000/";
+const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
-module.exports = async (env, options)  => {
+module.exports = async (env, options) => {
   const dev = options.mode === "development";
   const buildType = dev ? "dev" : "prod";
   const config = {
     devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      vendor: [
-        'react',
-        'react-dom',
-        'core-js',
-        'office-ui-fabric-react'
-      ],
-      taskpane: [
-        'react-hot-loader/patch',
-        './src/taskpane/index.js',
-      ],
-      commands: './src/commands/commands.js'
+      vendor: ["react", "react-dom", "core-js", "office-ui-fabric-react"],
+      taskpane: ["react-hot-loader/patch", "./src/taskpane/index.js"],
+      commands: "./src/commands/commands.js",
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".html", ".js"]
+      extensions: [".ts", ".tsx", ".html", ".js"],
     },
     module: {
       rules: [
@@ -47,60 +41,61 @@ module.exports = async (env, options)  => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.(png|jpg|jpeg|gif)$/,
           loader: "file-loader",
           options: {
-            name: '[path][name].[ext]',          
-          }
-        }
-      ]
-    },    
+            name: "[path][name].[ext]",
+          },
+        },
+      ],
+    },
     plugins: [
       new CleanWebpackPlugin(),
       new CopyWebpackPlugin({
         patterns: [
-        {
-          to: "taskpane.css",
-          from: "./src/taskpane/taskpane.css"
-        },
-        {
-          to: "[name]." + buildType + ".[ext]",
-          from: "manifest*.xml",
-          transform(content) {
-            if (dev) {
-              return content;
-            } else {
-              return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
-            }
-          }
-        }
-      ]}),
-      new ExtractTextPlugin('[name].[hash].css'),
+          {
+            from: "./src/taskpane/taskpane.css",
+            to: "taskpane.css",
+          },
+          {
+            from: "manifest*.xml",
+            to: "[name]." + buildType + ".[ext]",
+            transform(content) {
+              if (dev) {
+                return content;
+              } else {
+                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
+              }
+            },
+          },
+        ],
+      }),
+      new ExtractTextPlugin("[name].[hash].css"),
       new HtmlWebpackPlugin({
         filename: "taskpane.html",
-          template: './src/taskpane/taskpane.html',
-          chunks: ['taskpane', 'vendor', 'polyfill']
+        template: "./src/taskpane/taskpane.html",
+        chunks: ["taskpane", "vendor", "polyfills"],
       }),
       new HtmlWebpackPlugin({
-          filename: "commands.html",
-          template: "./src/commands/commands.html",
-          chunks: ["commands"]
+        filename: "commands.html",
+        template: "./src/commands/commands.html",
+        chunks: ["commands"],
       }),
       new webpack.ProvidePlugin({
-        Promise: ["es6-promise", "Promise"]
-      })
+        Promise: ["es6-promise", "Promise"],
+      }),
     ],
     devServer: {
       hot: true,
       headers: {
-        "Access-Control-Allow-Origin": "*"
-      },      
-      https: (options.https !== undefined) ? options.https : await devCerts.getHttpsServerOptions(),
-      port: process.env.npm_package_config_dev_server_port || 3000
-    }
+        "Access-Control-Allow-Origin": "*",
+      },
+      https: options.https !== undefined ? options.https : await devCerts.getHttpsServerOptions(),
+      port: process.env.npm_package_config_dev_server_port || 3000,
+    },
   };
 
   return config;
