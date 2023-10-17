e7f867a2be1e2f87ad60e5b4cea69e120337608e
Ricky Kirkham <rickki@microsoft.com>
2023-10-17 15:14:08 -0700

    adjust formatting to ensure all task pane space is used

--- src/taskpane/components/App.jsx
+++ src/taskpane/components/App.jsx
@@ -3,9 +3,17 @@ import PropTypes from "prop-types";
 import Header from "./Header";
 import HeroList from "./HeroList";
 import TextInsertion from "./TextInsertion";
+import { makeStyles } from "@fluentui/react-components";
 import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 
+const useStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+  }
+});
+
 const App = (props) => {
+  const styles = useStyles();
   // The list items are static and won't change at runtime,
   // so this should be an ordinary const, not a part of state.
   const listItems = [
@@ -24,7 +32,7 @@ const App = (props) => {
   ];
 
   return (
-    <div>
+    <div className={styles.root}>
       <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
       <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
       <TextInsertion />
