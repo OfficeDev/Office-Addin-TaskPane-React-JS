8048bcd4e8240166ebc0dde7b98d710aafba78e5
Rick Kirkham <Rick-Kirkham@users.noreply.github.com>
2023-10-18 12:10:45 -0700

    Merge update of React & Fluent to prerelease for testing (#107)
    * Bump yaml and postcss-loader
    
    Bumps [yaml](https://github.com/eemeli/yaml) to 2.2.2 and updates ancestor dependency [postcss-loader](https://github.com/webpack-contrib/postcss-loader). These dependencies need to be updated together.
    
    
    Updates `yaml` from 2.2.1 to 2.2.2
    - [Release notes](https://github.com/eemeli/yaml/releases)
    - [Commits](https://github.com/eemeli/yaml/compare/v2.2.1...v2.2.2)
    
    Updates `postcss-loader` from 4.3.0 to 7.2.4
    - [Release notes](https://github.com/webpack-contrib/postcss-loader/releases)
    - [Changelog](https://github.com/webpack-contrib/postcss-loader/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/webpack-contrib/postcss-loader/compare/v4.3.0...v7.2.4)
    
    ---
    updated-dependencies:
    - dependency-name: yaml
      dependency-type: indirect
    - dependency-name: postcss-loader
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * updated office-add-in-debugging and ran audit fix
    
    * Update to React18 and Fluent9 and other fixes (#106)
    
    * Update to React18 and Fluent9 and other fixes
    
    * temporarily remove IE and Edge Legacy tests
    
    * lint fix and add PropTypes
    
    * format change
    
    * adjust formatting to ensure all task pane space is used
    
    * lint fix
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Darren Miller <millerds@users.noreply.github.com>

---
+++ src/taskpane/components/Header.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Image, tokens, makeStyles } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  welcome__header: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingBottom: "30px",
+    paddingTop: "100px",
+    backgroundColor: tokens.colorNeutralBackground3,
+  },
+  message: {
+    fontSize: tokens.fontSizeHero900,
+    fontWeight: tokens.fontWeightRegular,
+    fontColor: tokens.colorNeutralBackgroundStatic,
+  },
+});
+
+const Header = (props) => {
+  const { title, logo, message } = props;
+  const styles = useStyles();
+
+  return (
+    <section className={styles.welcome__header}>
+      <Image width="90" height="90" src={logo} alt={title} />
+      <h1 className={styles.message}>{message}</h1>
+    </section>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+  logo: PropTypes.string,
+  message: PropTypes.string,
+};
+
+export default Header;
