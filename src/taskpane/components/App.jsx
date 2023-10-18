972b0f0a37d35d21f2fe7bfd4561efb9f073e902
Rick Kirkham <Rick-Kirkham@users.noreply.github.com>
2023-10-18 11:37:26 -0700

    Update to React18 and Fluent9 and other fixes (#106)
    * Update to React18 and Fluent9 and other fixes
    
    * temporarily remove IE and Edge Legacy tests
    
    * lint fix and add PropTypes
    
    * format change
    
    * adjust formatting to ensure all task pane space is used
    
    * lint fix

---
+++ src/taskpane/components/App.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Header from "./Header";
+import HeroList from "./HeroList";
+import TextInsertion from "./TextInsertion";
+import { makeStyles } from "@fluentui/react-components";
+import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
+
+const useStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+  },
+});
+
+const App = (props) => {
+  const styles = useStyles();
+  // The list items are static and won't change at runtime,
+  // so this should be an ordinary const, not a part of state.
+  const listItems = [
+    {
+      icon: <Ribbon24Regular />,
+      primaryText: "Achieve more with Office integration",
+    },
+    {
+      icon: <LockOpen24Regular />,
+      primaryText: "Unlock features and functionality",
+    },
+    {
+      icon: <DesignIdeas24Regular />,
+      primaryText: "Create and visualize like a pro",
+    },
+  ];
+
+  return (
+    <div className={styles.root}>
+      <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
+      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
+      <TextInsertion />
+    </div>
+  );
+};
+
+App.propTypes = {
+  title: PropTypes.string,
+};
+
+export default App;
---
+++ src/taskpane/components/HeroList.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { tokens, makeStyles } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  list: {
+    marginTop: "20px",
+  },
+  listItem: {
+    paddingBottom: "20px",
+    display: "flex",
+  },
+  icon: {
+    marginRight: "10px",
+  },
+  itemText: {
+    fontSize: tokens.fontSizeBase300,
+    fontColor: tokens.colorNeutralBackgroundStatic,
+  },
+  welcome__main: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  message: {
+    fontSize: tokens.fontSizeBase500,
+    fontColor: tokens.colorNeutralBackgroundStatic,
+    fontWeight: tokens.fontWeightRegular,
+    paddingLeft: "10px",
+    paddingRight: "10px",
+  },
+});
+
+const HeroList = (props) => {
+  const { items, message } = props;
+  const styles = useStyles();
+
+  const listItems = items.map((item, index) => (
+    <li className={styles.listItem} key={index}>
+      <i className={styles.icon}>{item.icon}</i>
+      <span className={styles.itemText}>{item.primaryText}</span>
+    </li>
+  ));
+  return (
+    <div className={styles.welcome__main}>
+      <h2 className={styles.message}>{message}</h2>
+      <ul className={styles.list}>{listItems}</ul>
+    </div>
+  );
+};
+
+HeroList.propTypes = {
+  items: PropTypes.array,
+  message: PropTypes.string,
+};
+
+export default HeroList;
